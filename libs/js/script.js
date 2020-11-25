4db46731b00b6a5c8dd8e75b56970354ebc03e82
dosed91 <sammorpeth@gmail.com>
2020-11-25 15:29:12 +0000

    Added functionality for finding a user by first name, retrieving all employee info from a department. Did a bit of styling for employee cards.

--- libs/js/script.js
+++ libs/js/script.js
@@ -37,7 +37,62 @@ $('#name-btn').on('click',function() {
     
     success: function(result) {
    
+      const employee = result['data'][0];
+      $('#employee-info-area').html('');
+      const employeeCardHTML = `
+      <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">${employee.firstName} ${employee.lastName}</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+      </div>`
+
+      $('#employee-info-area').append(employeeCardHTML);
+
+
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+
+      console.log(textStatus);
+      console.log(errorThrown);
+      console.log(jqXHR);
+    }
+  });
+}); 
+
+
+$('#department-select').on('change',function() {
+
+  $.ajax({
+    url: "libs/php/getAllByDepartment.php",
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      department: $('#department-select').val()
+    },
+    
+    success: function(result) {
+      $('#employee-info-area').html('');
+      // $('#department-title').html('');
       console.log(result['data']);
+      result['data'].forEach(employee => {
+        const employeeHTML = `<div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${employee.firstName} ${employee.lastName}</h5>
+          <ul>
+            <li>Email: ${employee.email}</li>
+            <li>Job Title: ${employee.jobTitle}</li>
+            <li>Department: ${employee.department}</li>
+            <li>Location: ${employee.location}</li>
+          </ul>
+        </div>
+        
+      </div>`
+       $('#employee-info-area').append(employeeHTML);
+      })
+      $('#department-title').html($('#department-select').val());
+
 
     },
     error: function(jqXHR, textStatus, errorThrown) {
--- libs/php/getAllByDepartment.php
+++ libs/php/getAllByDepartment.php
@@ -32,25 +32,12 @@
 
 	}	
 
-  // $query = 'SELECT firstName, lastName, jobTitle, email, departmentID 
-  //           FROM personnel
-
-  
-	// $query = 'SELECT p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location
-  //           FROM personnel p 
-  //           LEFT JOIN department d ON (d.id = p.departmentID)
-  //           LEFT JOIN location l ON (l.id = d.locationID) 
-  //           ORDER BY p.lastName, p.firstName, d.name, l.name
-  //           WHERE p.firstName = "' . $_REQUEST['firstName'] . '"';
-
-   
 	$query = 'SELECT p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location
             FROM personnel p 
             LEFT JOIN department d ON (d.id = p.departmentID)
-            LEFT JOIN location l ON (l.id = d.locationID) 
-            WHERE firstName = "' . $_REQUEST['firstName'] . '"
-            ORDER BY p.lastName, p.firstName, d.name, l.name
-           ';
+            LEFT JOIN location l ON (l.id = d.locationID)
+            WHERE d.name = "' . $_REQUEST['department'] . '"
+            ORDER BY p.lastName, p.firstName, d.name, l.name';
 
 	$result = $conn->query($query);
 	
